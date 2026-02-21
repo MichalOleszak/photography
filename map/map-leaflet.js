665b3152f9245fdf34ad46e9d191377d302b54e7
MichalOleszak <oleszak.michal@gmail.com>
2026-02-22 00:18:39 +0100

    update default zoom

--- map/map-leaflet.js
+++ map/map-leaflet.js
@@ -76,7 +76,7 @@
     // ───────────────────────────────────────────────
     var map = L.map('map-container', {
         center: [25, 20],
-        zoom: 2.75,
+        zoom: 2.6,
         minZoom: 2,
         maxZoom: 7,
         zoomControl: false,
